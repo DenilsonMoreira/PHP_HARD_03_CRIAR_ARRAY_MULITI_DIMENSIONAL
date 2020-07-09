1c109625d9f382be76ced2cb1dd543d27718d534
Denilsonk12 <43345513+Denilsonk12@users.noreply.github.com>
2020-07-09 12:38:45 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+$array_multidimensional = array(
+array(1,2),
+array(3,4),
+array(5,6)
+);
+
+// Utilizamos o <pre> pra melhorar a visualização da informação
+echo "<pre>";
+print_r($array_multidimensional);
+echo "</pre>";
+
+// essa informação será exibida em tela
+// Array
+// (
+//     [0] => Array
+//         (
+//             [0] => 1
+//             [1] => 2
+//         )
+
+//     [1] => Array
+//         (
+//             [0] => 3
+//             [1] => 4
+//         )
+
+//     [2] => Array
+//         (
+//             [0] => 5
+//             [1] => 6
+//         )
+
+// )
+?>
